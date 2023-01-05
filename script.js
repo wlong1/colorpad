098b88114e04b20fcdc06567ccf4cdb00fc0ebec
wlong1 <109259315+wlong1@users.noreply.github.com>
2023-01-04 21:00:38 -0500

    Updated visuals
    Changed font and decreased grid size

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const gridBox = document.querySelector('.gridbox');
 let number;
+const gridBox = document.querySelector('.gridbox');
+const resizeBtn = document.querySelector('#resizeBtn');
+const clearBtn = document.querySelector('#clearBtn');
+
+
+function loadGrid(num){
+  gridBox.replaceChildren(); // Clear grid
+  gridBox.style.gridTemplateColumns = 'repeat('+num+',' +600/num+'px)';
+  gridBox.style.gridTemplateRows = 'repeat('+num+',' +600/num+'px)';
+  for (i = 0; i < num*num; i++){
+    let temp = document.createElement('div');
+    temp.classList.add('gridElement');
+    temp.addEventListener('mouseover', (e) => e.target.style.backgroundColor = colorTune(getComputedStyle(e.target).backgroundColor));
+    gridBox.appendChild(temp);
+  };
+  number = num;
+};
+
+
+function gridCount(){
+  number = parseInt(prompt("Enter grid width (Maximum of 50):"));
+  if (number == null || number == "" || number >50){
+    number = 50;
+  } else if (number < 1){
+    number = 1;
+  };
+  loadGrid(number);
+};
 
 
 function rgb2hex(rgb) {
@@ -13,7 +40,6 @@ function rgb2hex(rgb) {
 
 function HexToHSL(hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
   var r = parseInt(result[1], 16);
   var g = parseInt(result[2], 16);
   var b = parseInt(result[3], 16);
@@ -28,6 +54,7 @@ function HexToHSL(hex) {
   return l;
 }
 
+
 function colorTune(target){
   let l = HexToHSL(rgb2hex(target));
   h = Math.floor(Math.random() * 360);
@@ -35,36 +62,10 @@ function colorTune(target){
   if (l < 0){
     l = 0;
   };
-  return 'hsl('+h+',30%,'+l+'%)';
+  return 'hsl('+h+',50%,'+l+'%)';
 };
 
-function loadGrid(num){
-  gridBox.replaceChildren(); // Clear grid
-  gridBox.style.gridTemplateColumns = 'repeat('+num+',' +960/num+'px)';
-  gridBox.style.gridTemplateRows = 'repeat('+num+',' +960/num+'px)';
-  for (i = 0; i < num*num; i++){
-    let temp = document.createElement('div');
-    temp.classList.add('gridElement');
-    temp.addEventListener('mouseover', (e) => e.target.style.backgroundColor = colorTune(getComputedStyle(e.target).backgroundColor));
-    gridBox.appendChild(temp);
-  };
-  number = num;
-};
 
 loadGrid(20);
-
-const resizeBtn = document.querySelector('#resizeBtn');
 resizeBtn.addEventListener('click', gridCount);
-
-function gridCount(){
-  number = parseInt(prompt("Enter grid width (Maximum of 100):"));
-  if (number == null || number == "" || number >100){
-    number = 100;
-  } else if (number < 1){
-    number = 1;
-  };
-  loadGrid(number);
-};
-
-const clearBtn = document.querySelector('#clearBtn');
 clearBtn.addEventListener('click', () => loadGrid(number));
